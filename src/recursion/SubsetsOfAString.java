f4ff8cc06d73eeee8d80721710a2c6df68043be6
jacky-ho0429 <jacky.ho25@ycdsbk12.ca>
2025-05-17 17:00:39 -0400

    Redid SubsetsOfAString.java As It Was A Incorrect Implementation

--- src/recursion/SubsetsOfAString.java
+++ src/recursion/SubsetsOfAString.java
@@ -1,14 +1,25 @@
 package recursion;
+
 public class SubsetsOfAString {
 
     public static void generateSubsets(String s) {
-        for (int i = 0; i <= s.length(); i++) { 
-            String subsets = s.substring(0,i);
-            System.out.println(subsets);
+        for (int length = 1; length <= s.length(); length++) {
+            generateSubsets(s, "", 0, length);
         }
-        if (s.length() > 1) {
-            generateSubsets(s.substring(1,s.length()));
+    }
+
+    private static void generateSubsets(String s, String current, int index, int targetLength) {
+        if (current.length() == targetLength) {
+            System.out.println(current);
+            return;
         }
+        if (index == s.length()) {
+            return;
+        }
+        
+        generateSubsets(s, current + s.charAt(index), index + 1, targetLength);
+
+        generateSubsets(s, current, index + 1, targetLength);
     }
 
     public static void main(String[] args) {
